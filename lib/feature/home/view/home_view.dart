f0f2b7f5784d2dcf14da8cf83b8bbcf0779b169a
arkannurszzz <arkannurszzz@gmail.com>
2025-10-28 18:11:07 +0700

    update foldering

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -13,38 +13,16 @@ class HomeView extends StatelessWidget {
       builder: (controller) {
         return Scaffold(
           backgroundColor: Colors.white,
-          body: Stack(
-            children: [
-              Stack(
-                children: [
-                  Image.asset(
-                    'assets/image/topappbar_home.png',
-                    width: double.infinity,
-                    fit: BoxFit.fill,
-                    alignment: Alignment.topCenter,
-                  ),
-                ],
-              ),
-              // beri padding/top margin agar konten tidak tertutup
-              Positioned.fill(
-                top: 200, // tinggi gambar
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      const Text(
-                        'Login',
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          body: Padding(
+            padding: const EdgeInsets.all(0.0),
+            child: Image.asset(
+              'assets/image/topappbar_home.png',
+              width: double.infinity,
+              fit: BoxFit.fill,
+              alignment: Alignment.topCenter,
+            ),
           ),
+          floatingActionButton: Card(),
         );
       },
     );
